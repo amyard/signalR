6ea65d9f887d028af67400a66a141340a73d1ea0
Maksym Turenko <maksym@bundledocs.com>
2023-03-12 18:36:02 +0000

    create UI for create Room modal

--- ChatApp/wwwroot/js/site.js
+++ ChatApp/wwwroot/js/site.js
@@ -1,1 +1,10 @@
-﻿
+﻿var createRoomButton = document.getElementById("create-room-btn");
+var createRoomModal = document.getElementById("create-room-modal");
+
+createRoomButton.addEventListener("click", function() {
+    createRoomModal.classList.add("active");
+})
+
+function closeModal() {
+    createRoomModal.classList.remove("active");
+}

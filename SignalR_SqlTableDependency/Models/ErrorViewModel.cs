d5c154bd8186f1bc8a5549983c982bcd5120272e
Maksym Turenko <maksym@bundledocs.com>
2023-03-14 21:37:17 +0000

    git init.

---
+++ SignalR_SqlTableDependency/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SignalR_SqlTableDependency.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
